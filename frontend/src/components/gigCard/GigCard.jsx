d6e47dcdcb2e46092f06b00a8f548b0856d50b71
Malindu Delpitiya <mdelpitiya66@gmail.com>
2024-06-13 00:36:08 +0530

    ....

--- frontend/src/components/gigCard/GigCard.jsx
+++ frontend/src/components/gigCard/GigCard.jsx
@@ -1,72 +1,40 @@
 import React from "react";
 import "./GigCard.scss";
-import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import {Link} from "react-router-dom";
+import {useQuery} from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 import "bootstrap/dist/css/bootstrap.min.css";
-import img from './../../../public/img/sobbg.png'
+import img from "./../../../public/img/sobbg.png";
 
-const GigCard = ({ item }) => {
-  const { isLoading, error, data } = useQuery({
+const GigCard = ({item}) => {
+  const {isLoading, error, data} = useQuery({
     queryKey: [item.userId],
     queryFn: () =>
-      newRequest.get(`/users/${item.userId}`).then((res) => {
-        return res.data;
-      }),
+      newRequest.get(`/users/${item.userId}`).then((res) => res.data),
   });
-  console.log("data", data);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data</div>;
+ console.log("item", item);
   return (
-    // <Link to={`/gig/${item._id}`} className="link">
-    //   <div className="gigCard">
-    //     <img src={item.cover} alt="" />
-    //     <div className="info">
-    //       {isLoading ? (
-    //         "loading"
-    //       ) : error ? (
-    //         "Something went wrong!"
-    //       ) : (
-    //         <div className="user">
-    //           <img src={data.img || "/img/noavatar.jpg"} alt="" />
-    //           <span>{data.username}</span>
-    //         </div>
-    //       )}
-    //       <p>{item.title}</p>
-    //       <div className="star">
-    //         <img src="./img/star.png" alt="" />
-    //         <span>
-    //           {!isNaN(item.totalStars / item.starNumber) &&
-    //             Math.round(item.totalStars / item.starNumber)}
-    //         </span>
-    //       </div>
-    //     </div>
-    //     <hr />
-    //     <div className="detail">
-    //       <img src="./img/heart.png" alt="" />
-    //       <div className="price">
-    //         <span>STARTING AT</span>
-    //         <h2>$ {item.price}</h2>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Link>
     <Link to={`/gig/${item._id}`} className="link">
-      <div class="card gigCard">
-        <img src={item.cover} class="card-img-top cover" alt="..." />
-        <div class="card-body">
+      <div className="card gigCard">
+        <img src={item.cover} className="card-img-top cover" alt="Cover" />
+        <div className="card-body">
           <div className="d-flex flex-row info">
             <div className="user">
-              <img src={item.img || img} alt="" />
-              <span>{data.username}</span>
+              <img src={item.img || img} alt="User" />
+              <span>{item.username || item.title}</span>
             </div>
           </div>
           <div className="title">
-            <p class="card-text">{item.title}</p>
+            <p className="card-text">{item.title}</p>
           </div>
         </div>
-        <ul class="list-group list-group-flush">
+        <ul className="list-group list-group-flush">
           <div className="info">
             <div className="star">
-              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="Star" />
               <span>
                 {!isNaN(item.totalStars / item.starNumber) &&
                   Math.round(item.totalStars / item.starNumber)}
@@ -74,7 +42,7 @@ const GigCard = ({ item }) => {
             </div>
           </div>
         </ul>
-        <div class="card-body">
+        <div className="card-body">
           <div className="price d-flex justify-content-between">
             <span>STARTING AT</span>
             <h2>$ {item.price}</h2>
